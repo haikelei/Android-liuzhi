70e28c8e8771aae8e2e3d16f229200e151d1e4c1
zhaolongtao <13295815771@163.com>
2018-11-19 21:04:07 +0800

    登录注册等

--- app/src/main/java/com/hykj/liuzhi/androidcomponents/net/http/ApiConstant.java
+++ app/src/main/java/com/hykj/liuzhi/androidcomponents/net/http/ApiConstant.java
@@ -1,9 +1,9 @@
 package com.hykj.liuzhi.androidcomponents.net.http;
 
-public interface ApiConstant {
+public class ApiConstant {
     //正式
-  public static String ROOT_URL = "http://101.37.145.45";
-  public static String LOGIN = "/api/index/register";
-  public static String REGISTER = "/api/index/register";
-  public static String GETUSERSELF = "/api/index/getuserself";
+  public static final String ROOT_URL = "http://101.37.145.45";
+  public static final String LOGIN = "/api/index/login";
+  public static final String REGISTER = "/api/index/register";
+  public static final String GETUSERSELF = "/api/index/getuserself";
 }
